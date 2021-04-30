d10ec5b34a3d6e2147b15d7404259af2a2b490a6
natak0 <bcnatako@gmail.com>
2021-04-30 15:21:58 +0200

    add transitions

--- assets/updatePattern.js
+++ assets/updatePattern.js
@@ -16,18 +16,35 @@ function click() {
 function update(data) {
     svg
         .selectAll('text')
-        .data(data, d=>d)//control the update with the key function (check for the repeated content)
+        .data(data, d => d)//control the update with the key function (check for the repeated content)
         .join(
-            enter => enter
-                .append('text')
-                .text(d => d)
-                .attr('x', 30)
-                .attr('y', (d, i) => i * 30 + 50)
-                .style('fill', 'blue'),
-            update => update
-                .style('fill', 'gray')
-                .attr('y', (d, i) => i * 30 + 50),
-            exit => exit.remove()
+            enter => {
+                enter
+                    .append('text')
+                    .text(d => d)
+                    .attr('x', -30)
+                    .attr('y', (d, i) => i * 30 + 50)
+                    .style('fill', 'blue')
+                    //to return a selection not a transition
+                    /*   .call(enter => enter
+                        .transition()
+                        .attr('x',30)), */
+                    .transition()
+                    .attr('x', 30)
+                },
+            update => {
+                update
+                    .transition()
+                    .style('fill', 'gray')
+                    .attr('y', (d, i) => i * 30 + 50)
+            },
+            exit => {
+                exit
+                    .transition()
+                    .attr('x', 200)
+                    .style('fill', 'tomato')
+                    .remove()
+            }
         )
 }
 
